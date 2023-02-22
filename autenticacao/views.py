3543c13f87b378d5e977f9f27f6961b6b29768a4
Esley <esleynathan@hotmail.com>
2023-02-21 22:43:29 -0300

    Freelaway | Menssagens de erro, Exibir mensagens no tempalte HTML.

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def cadastro(request):
     if request.method == "GET":
@@ -11,15 +13,17 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirm-password')
 
     if not senha == confirmar_senha:
+        messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
         return redirect('/auth/cadastro')
 
     if len(username.strip()) == 0 or len(senha.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
         return redirect('/auth/cadastro')
-    
+
     user = User.objects.filter(username=username)
     if user.exists():
-        return redirect('/auth/cadastro')
-    
+        messages.add_message(request, constants.ERROR, 'Já existe um usário com esse username')
+        
     try:
         user = User.objects.create_user(username=username, password=senha)
         user.save()
